357b57432a7c7b4164bc4993190c997eba263039
briantwhelan <72137130+briantwhelan@users.noreply.github.com>
2021-04-18 16:03:12 +0100

    Added Pavel's Merge Sort code (not integrated yet though)

--- FinalProgram/utility/MergeSort.java
+++ FinalProgram/utility/MergeSort.java
@@ -1,15 +1,19 @@
 package utility;
 
+import java.util.Arrays;
+
 /*************************************************************************
  *  Merge Sort class (for CSU22012 Project).
  *
- *  @version 1.0 12/4/21
+ *  @version 2.0 18/4/21
  *
- *  @author Brian Whelan
+ *  @author Brian Whelan (improvements have been made by integrating Pavel 
+ *  					  Petrukhin's assignment 1 merge sort improvements)
  *
  *************************************************************************/
 public class MergeSort 
 {
+	private static int CUTOFF = 10;
 	/**
      * Don't let anyone instantiate this class.
      */
@@ -78,4 +82,92 @@ public class MergeSort
     		else 									array[k] = aux[i++];
     	}		
     }
+    
+    
+    
+    
+    
+//    //Code below adapted from Pavel Petrukhin's assignment 1
+//    private static void swap(StopTime[] a, int i, int j)
+//    {
+//          StopTime temp = a[i];
+//          a[i] = a[j];
+//          a[j] = temp;
+//    }
+//    
+//
+//    private static void cutoffInsertion(StopTime[] a, int low, int high)
+//    {
+//       for(int i = low; i < high; i++)
+//       {
+//          for(int j = i + 1; j > low; j--)
+//          {
+//             if(a[j].compareTo(a[j - 1]) < 0)
+//             {
+//                StopTime temp = a[j];
+//                a[j] = a[j - 1];
+//                a[j - 1] = temp;
+//             }
+//             else
+//             {
+//                break;
+//             }
+//          }
+//       }
+//    }
+//    
+//    private static void merge(StopTime[] a, StopTime[] aux, int left, int mid, int right)
+//    {
+//       //note we copy into aux, so we have to launch with aux and a swapped (to get final array in a)
+//       int i = left,j = mid + 1;
+//       for(int k = left; k <= right; k++)
+//       {
+//          if(i > mid)
+//          {
+//             aux[k] = a[j++];
+//          }
+//          else if(j > right)
+//          {
+//             aux[k] = a[i++];
+//          }
+//          else if(a[j].compareTo(a[i]) < 0)
+//          {
+//             aux[k] = a[j++];
+//          }
+//          else
+//          {
+//             aux[k] = a[i++];
+//          }
+//       }
+//    }
+//
+//    private static void mergeSortRecursive(StopTime[] a, StopTime[] aux, int l, int r)
+//    {
+//       if(r - l > CUTOFF) 
+//       {
+//          int m = l + (r - l)/2;
+//          mergeSortRecursive(aux, a,l,m);       //swapping current aux and a, so that a is aux param in next call
+//          mergeSortRecursive(aux, a,m+1,r);
+//          if(a[m].compareTo(a[m+1]) <= 0) //in case we do not have to merge, we still have to copy into aux
+//          {                      
+//            System.arraycopy(a,l,aux,l, r - l + 1);
+//            return;
+//          }
+//          merge(a,aux, l, m, r);                   //merge happens into aux, so we have a normal order here, since first
+//                                                   //call is swapped (we want 2 big arrays to be merged in our initial array
+//                                                   //which is an aux parameter in first call
+//       }
+//       else
+//       {
+//          cutoffInsertion(aux,l,r);                //we sort aux, since we always merge into aux and cutoff should have
+//                                                   //the same effect as merging
+//       }
+//    }
+//
+//    static StopTime[] mergeSort(StopTime[] a) 
+//    {
+//       StopTime[] aux =   Arrays.copyOf(a,a.length);
+//       mergeSortRecursive(aux,a,0,a.length-1); //start with swapped since things are copied into aux param in merge
+//       return a;
+//    }
 }
